022677374621342dc285fb86530259b8289a48a0
Jonathan DeMasi <jrdemasi@gmail.com>
2022-07-14 00:27:24 -0600

    add management node, some cool logic, init

--- terraform/modules/vultr_zfs_node/main.tf
+++ terraform/modules/vultr_zfs_node/main.tf
@@ -13,23 +13,53 @@ provider "vultr" {
   retry_limit = 3
 }
 
-resource "vultr_vpc" "devstack_vpc" {
-    description = "devstack vpc"
+resource "vultr_vpc" "storage_dev_env_vpc" {
+    description = "storage_dev_env vpc"
     region = "ewr"
 }
 
-resource "vultr_firewall_group" "devstack_internal" {
-    description = "Firewall group for devstack internal nodes"
+# Internal storage node firewall
+resource "vultr_firewall_group" "storage_dev_env_internal" {
+    description = "Firewall group for storage_dev_env internal nodes"
 }
 
-resource "vultr_firewall_rule" "allow_all_devstack" {
-    firewall_group_id = vultr_firewall_group.devstack_internal.id
+resource "vultr_firewall_rule" "allow_all_storage_dev_env" {
+    firewall_group_id = vultr_firewall_group.storage_dev_env_internal.id
     protocol = "tcp"
     ip_type = "v4"
-    subnet = "${vultr_vpc.devstack_vpc.v4_subnet}"
-    subnet_size = "${vultr_vpc.devstack_vpc.v4_subnet_mask}"
-    notes = "Allow all within devstack vpc"
+    subnet = "${vultr_vpc.storage_dev_env_vpc.v4_subnet}"
+    subnet_size = "${vultr_vpc.storage_dev_env_vpc.v4_subnet_mask}"
+    notes = "Allow all within storage_dev_env vpc"
     port = "1:65535"
+}
+
+# Management node firewall, which includes public SSH access 
+resource "vultr_firewall_group" "storage_dev_env_mgmt" {
+    description = "Firewall group for storage_dev_env management node(s)"
+}
+
+resource "vultr_firewall_rule" "allow_all_storage_dev_env" {
+    firewall_group_id = vultr_firewall_group.storage_dev_env_mgmt.id
+    protocol = "tcp"
+    ip_type = "v4"
+    subnet = "${vultr_vpc.storage_dev_env_vpc.v4_subnet}"
+    subnet_size = "${vultr_vpc.storage_dev_env_vpc.v4_subnet_mask}"
+    notes = "Allow all within storage_dev_env vpc"
+    port = "1:65535"
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+resource "vultr_firewall_rule" "allow_all_storage_dev_env" {
+    firewall_group_id = vultr_firewall_group.storage_dev_env_mgmt.id
+    protocol = "tcp"
+    ip_type = "v4"
+    subnet = "${chomp(data.http.myip.body)}
+    subnet_size = "32"
+    notes = "Allow SSH from terraform execution location"
+    port = "22"
 }
 
 resource "vultr_instance" "zfs00_instance" {
@@ -37,14 +67,14 @@ resource "vultr_instance" "zfs00_instance" {
     region = "ewr"
     os_id = 1743 # Ubuntu 22.04 LTS
     label = "zfs00"
-    tags = ["devstack"]
+    tags = ["storage_dev_env"]
     hostname = "zfs00.vultr.jthan.io"
     enable_ipv6 = true
     backups = "disabled"
     ddos_protection = false
     activation_email = false
-    vpc_ids = ["${vultr_vpc.devstack_vpc.id}"]
-    firewall_group_id = vultr_firewall_group.devstack_internal.id
+    vpc_ids = ["${vultr_vpc.storage_dev_env_vpc.id}"]
+    firewall_group_id = vultr_firewall_group.storage_dev_env_internal.id
     user_data = "${file("files/zfs-cloud-init.sh")}"
 }
 
@@ -69,14 +99,14 @@ resource "vultr_instance" "zfs01_instance" {
     region = "ewr"
     os_id = 1743 # Ubuntu 22.04 LTS
     label = "zfs01"
-    tags = ["devstack"]
+    tags = ["storage_dev_env"]
     hostname = "zfs01.vultr.jthan.io"
     enable_ipv6 = true
     backups = "disabled"
     ddos_protection = false
     activation_email = false
-    vpc_ids = ["${vultr_vpc.devstack_vpc.id}"]
-    firewall_group_id = vultr_firewall_group.devstack_internal.id
+    vpc_ids = ["${vultr_vpc.storage_dev_env_vpc.id}"]
+    firewall_group_id = vultr_firewall_group.storage_dev_env_internal.id
     user_data = "${file("files/zfs-cloud-init.sh")}"
 }
 
@@ -95,4 +125,20 @@ resource "vultr_block_storage" "zfs01_vol1" {
     block_type = "storage_opt"
     live   = true
     attached_to_instance = vultr_instance.zfs01_instance.id
+}
+
+resource "vultr_instance" "mgmt00_instance" {
+    plan = "vc2-1c-1gb"
+    region = "ewr"
+    os_id = 1743 # Ubuntu 22.04 LTS
+    label = "mgmt00"
+    tags = ["storage_dev_env"]
+    hostname = "mgmt00.vultr.jthan.io"
+    enable_ipv6 = true
+    backups = "disabled"
+    ddos_protection = false
+    activation_email = false
+    vpc_ids = ["${vultr_vpc.storage_dev_env_vpc.id}"]
+    firewall_group_id = vultr_firewall_group.storage_dev_env_mgmt.id
+    user_data = "${file("files/mgmt-cloud-init.sh")}"
 }
