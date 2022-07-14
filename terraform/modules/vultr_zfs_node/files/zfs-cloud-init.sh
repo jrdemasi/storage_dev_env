f1fee44847a46906409c2317b3635deb04d08b52
Jonathan DeMasi <jrdemasi@gmail.com>
2022-07-13 22:24:47 -0600

    typo, add playbook

--- terraform/modules/vultr_zfs_node/files/zfs-cloud-init.sh
+++ terraform/modules/vultr_zfs_node/files/zfs-cloud-init.sh
@@ -3,4 +3,5 @@
 apt update 
 apt install -y git ansible # ensure we have git to clone our repo and let ansible take over
 cd /root
-git glone https://github.com/jrdemasi/storage_dev_env # repo contains playbooks
+git clone https://github.com/jrdemasi/storage_dev_env # repo contains playbooks
+ansible-playbook /root/storage_dev_env/ansible/playbooks/zfs.yml
